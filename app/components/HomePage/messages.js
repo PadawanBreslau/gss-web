9556001c0fecb13bc04b8a9df12869408d0f3a1e
Staszek <s.zawadzki@visuality.pl>
2020-11-02 16:01:11 +0100

    Home page

--- app/components/HomePage/messages.js
+++ app/components/HomePage/messages.js
@@ -10,6 +10,10 @@ export const scope = 'app.containers.HomePage';
 export default defineMessages({
   header: {
     id: `${scope}.header`,
-    defaultMessage: 'Tet',
+    defaultMessage: 'Strona wirtualnego szlaku przez całe Sudety',
+  },
+  lead: {
+    id: `${scope}.lead`,
+    defaultMessage: '500km trasy, 20km przewyższenia, mnóstwo gór, historii i bez nadmiernych asfaltów',
   },
 });
--- app/containers/HomePage/index.js
+++ app/containers/HomePage/index.js
@@ -1,11 +1,8 @@
 import React from 'react';
-import { FormattedMessage } from 'react-intl';
-import messages from './messages';
+import HomePageComponent from 'components/HomePage';
 
 export default function HomePage() {
   return (
-    <h1>
-      <FormattedMessage {...messages.header} />
-    </h1>
+    <HomePageComponent />
   );
 }
