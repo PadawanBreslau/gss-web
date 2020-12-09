f93c865530704fe02edd19110159f1274b35438f
Staszek <s.zawadzki@visuality.pl>
2020-12-09 12:43:54 +0100

    Trivi with location

--- app/components/Trivium/index.js
+++ app/components/Trivium/index.js
@@ -1,11 +1,25 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { ZoomOutMap } from '@material-ui/icons';
+
 import './trivia.scss';
 
 const Trivium = ({ trivium }) => (
   <div className="trivium-box">
     {trivium.content}
 
+    {trivium.lat && trivium.lon && (
+      <a
+        href={`https://mapa-turystyczna.pl/coords/${trivium.lat},${trivium.lon}#${trivium.lat}/${
+          trivium.lon
+        }/17`}
+        target="_blank"
+      >
+        <span className="openMap">
+          <ZoomOutMap />
+        </span>
+      </a>
+    )}
   </div>
 );
 
