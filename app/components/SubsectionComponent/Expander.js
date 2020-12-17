e2fa2be7c75db2913af15fdf21f94dc2ea052e6d
Staszek <s.zawadzki@visuality.pl>
2020-12-17 13:01:28 +0100

    Subsection refactor

---
+++ app/components/SubsectionComponent/Expander.js
@@ -0,0 +1,20 @@
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import { ChevronRight, ChevronLeft } from '@material-ui/icons';
+
+const Expander = ({ setter, value, label }) => (
+  <div className="expandable-title" onClick={() => setter(!value)}>
+    <div className="section-chevron">
+      {value ? <ChevronLeft fontSize="large" /> : <ChevronRight fontSize="large" />}
+    </div>
+    <div>{label}</div>
+  </div>
+);
+
+Expander.propTypes = {
+  setter: PropTypes.func,
+  value: PropTypes.bool,
+  label: PropTypes.string,
+};
+
+export default Expander;
--- app/components/SubsectionComponent/index.js
+++ app/components/SubsectionComponent/index.js
@@ -1,72 +1,83 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
-import { Link } from 'react-router-dom';
+import { ChevronRight, ChevronLeft } from '@material-ui/icons';
 import Utility from '../Utility';
 import Trivium from '../Trivium';
-import Info from '../Info';
 import MapaTurystyczna from '../MapaTurystyczna';
 import Image from '../Image';
+import Expander from './Expander';
 import './subsection.scss';
 
-const SubsectionComponent = ({ data }) => (
-  <>
-    <div className="subsection-view">
-      <div className="subsection-view-title">
-        {data.start} - {data.finish}
-      </div>
-      {false && (
-        <div className="subsection-data">
-          <Info
-            length={data.length}
-            ascent={data.ascent}
-            descent={data.descent}
-            trackColor={data.trackColor}
-          />
+const SubsectionComponent = ({ data }) => {
+  const [utilitiesVisible, setUtilitiesVisible] = useState(false);
+  const [triviaVisible, setTriviaVisible] = useState(false);
+  const [mapVisible, setMapVisible] = useState(false);
+
+  return (
+    <>
+      <div className="subsection-view">
+        <div className="subsection-view-title">
+          {data.start} - {data.finish}
         </div>
-      )}
-      <div className="subsection-description-with-photo">
-        {data.images && data.images.length > 0 && data.images[0] !== undefined && (
-          <div className="float">
-            {' '}
-            <Image image={data.images[0]} />{' '}
+        <div className="subsection-description-with-photo">
+          {data.images && data.images.length > 0 && data.images[0] !== undefined && (
+            <div className="float">
+              {' '}
+              <Image image={data.images[0]} />{' '}
+            </div>
+          )}
+          {descriptionToArray(data.description)?.map((block) => (
+            <div className="text-block" key={block}>
+              {block}
+            </div>
+          ))}
+        </div>
+
+        <Expander setter={setMapVisible} value={mapVisible} label={"Mapa"} />
+
+        {mapVisible && (
+          <div className="subsection-mt">
+            {data.mtUuid && (
+              <MapaTurystyczna mtUuid={data.mtUuid} horizontalMap={data.horizontalMap} />
+            )}
           </div>
         )}
-        {descriptionToArray(data.description)?.map((block) => (
-          <div className="text-block" key={block}>
-            {block}
-          </div>
-        ))}
-      </div>
-      <div
-        className={
-          data.horizontalMap
-            ? 'subsection-map-utilities-horizontal'
-            : 'subsection-map-utilities-vertical'
-        }
-      >
-        <div className="subsection-mt">
-          {data.mtUuid && (
-            <MapaTurystyczna mtUuid={data.mtUuid} horizontalMap={data.horizontalMap} />
+
+        <Expander setter={setUtilitiesVisible} value={utilitiesVisible} label={"Noclegi, jedzenie i inne"} />
+
+       { utilitiesVisible &&
+        <div className="trivia-and-utilities">
+          {data.utilities && (
+            <div className="subsection-utilities">
+              {data.utilities.map((utility) => (
+                <Utility utility={utility} key={utility.id} />
+              ))}
+            </div>
           )}
         </div>
-        <div className="trivia-and-utilities">
-          {data.utilities && <div className="subsection-utilities">
-            {data.utilities.map((utility) => <Utility utility={utility} key={utility.id} />)}
-          </div>
-          }
-          <div className="subsection-trivia">
-            {data.trivia && data.trivia.length > 0 &&
+        }
+
+        <Expander setter={setTriviaVisible} value={triviaVisible} label={"Ciekawostki"} />
+
+
+        { triviaVisible &&
+        <div className="subsection-trivia">
+          {data.trivia && data.trivia.length > 0 && (
+            <>
+              <span className="triviaLabel">Ciekawostki</span>
               <>
-                <span className="triviaLabel">Ciekawostki</span>
-                <>{data.trivia.map((trivium) => <Trivium trivium={trivium} key={trivium.id} />)}</>
+                {data.trivia.map((trivium) => (
+                  <Trivium trivium={trivium} key={trivium.id} />
+                ))}
               </>
-            }
-          </div>
+            </>
+          )}
         </div>
+        }
       </div>
-    </div>
-  </>
-);
+    </>
+  );
+};
 
 const descriptionToArray = (description) => description?.split('\n');
 
