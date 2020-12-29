51bdb15e3f8ab5a9cf7ed2963f2cd8d5e0c2861b
Staszek <s.zawadzki@visuality.pl>
2020-12-29 15:48:15 +0100

    Downloading routes

--- app/components/HomePage/index.js
+++ app/components/HomePage/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import { FormattedMessage } from 'react-intl';
-import { Star } from '@material-ui/icons';
+import { Star, GetApp } from '@material-ui/icons';
 import messages from './messages';
+import config from '../../config';
 import './homepage.scss';
 
 const HomePage = () => (
@@ -16,11 +17,17 @@ const HomePage = () => (
     <div className="home-mt">
       <iframe
         title="Mapa turystyczna"
-        src="https://mapa-turystyczna.pl/map/widget/route/h1l0p0/u9gl.html"
+        src="https://mapa-turystyczna.pl/map/widget/route/h1l0p1/u9gl.html"
         height="660"
         frameBorder="0"
         width="100%"
       />
+    </div>
+    <div className='home-downloads'>
+      <div className='home-track'><a href='/gss20-full-official.gpx'><GetApp />Całość trasy GSS 2.0</a></div>
+      <div className='home-track'><a href='/gss20-bardo-duszniki-official.gpx'><GetApp />GSS 2.0: Bardo - Duszniki-Zdrój</a></div>
+      <div className='home-track'><a href='/gss20-duszniki-mieroszow-official.gpx'><GetApp />GSS 2.0: Duszniki-Zdrój - Mieroszów</a></div>
+      <div className='home-track'><a href='/gss20-mieroszow-czerniawa-official.gpx'><GetApp />GSS 2.0: Mieroszów - Czernica-Zdrój</a></div>
     </div>
     <div className="home-list">
       <div className="home-list-element">
