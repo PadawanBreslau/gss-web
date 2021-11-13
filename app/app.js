d75af51a516fbc0c70e19c2068c6ae075f79d77d
Staszek <s.zawadzki@visuality.pl>
2021-11-13 11:15:10 +0100

    Add GTM module

--- app/app.js
+++ app/app.js
@@ -28,18 +28,17 @@ import '!file-loader?name=[name].[ext]!./images/favicon.ico';
 import 'file-loader?name=.htaccess!./.htaccess';
 /* eslint-enable import/no-unresolved, import/extensions */
 
+import TagManager from 'react-gtm-module';
 import configureStore from './configureStore';
 
 // Import i18n messages
 import { translationMessages } from './i18n';
-import TagManager from 'react-gtm-module';
 
 // Create redux store with history
 const initialState = {};
 const store = configureStore(initialState, history);
 const MOUNT_NODE = document.getElementById('app');
-TagManager.initialize({gtmId: 'GTM-T78HQHW' })
-
+TagManager.initialize({ gtmId: 'GTM-T78HQHW' });
 
 const render = (messages) => {
   ReactDOM.render(
