f5aa4f57088bf975e2aa6e72fc903e6669609bcd
Staszek <s.zawadzki@visuality.pl>
2022-01-20 20:30:15 +0100

    Show subsection

--- app/components/SectionsComponent/Section.js
+++ app/components/SectionsComponent/Section.js
@@ -9,15 +9,25 @@ import './sections.scss';
 
 const Section = ({ section }) => {
   const { variation } = section;
+  const [subsectionVisible, setSubsectionVisible] = useState(false);
   const className = `section-${variation}`;
 
   return (
     <div className={className}>
       <div className="title">
-        <div className="section-title">
-          <div className="section-start-end">
-            <div className="section-location">{section.start}</div>
-            <div className="section-location">{section.finish}</div>
+        <div className="expandable-title" onClick={() => setSubsectionVisible(!subsectionVisible)}>
+          <div className="section-title">
+            <div className="section-chevron">
+              {!subsectionVisible ? (
+                <ChevronRight fontSize="large" />
+              ) : (
+                <ChevronLeft fontSize="large" />
+              )}
+            </div>
+            <div className="section-start-end">
+              <div className="section-location">{section.start}</div>
+              <div className="section-location">{section.finish}</div>
+            </div>
           </div>
         </div>
         <div className="allinfo">
@@ -41,7 +51,7 @@ const Section = ({ section }) => {
           </span>
         )}
       </div>
-      {false && (
+      {subsectionVisible && (
         <div className="sectionWithMap">
           <div className="subsectionWithDescription">
             <div className="subsections">
