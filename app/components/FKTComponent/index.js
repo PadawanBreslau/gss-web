a70b7508de69f1079186fb5b943e5af208dc88b3
Staszek <s.zawadzki@visuality.pl>
2022-07-04 15:22:14 +0200

    FKT update

--- app/components/FKTComponent/index.js
+++ app/components/FKTComponent/index.js
@@ -11,10 +11,31 @@ const FKTComponent = () => (
       <h3>Kierunek Bardo-Świeradów</h3>
       <ul>
         <li>
+          <span className='fkt-who'><a href='https://www.facebook.com/StrefaWedrowcy/' target='_blank'>Piotr Piasecki</a></span>
+          <span className='fkt-how-much'>17d 10h</span>
+          <span className='fkt-when'>Czerwiec, 2022</span>
+        </li>
+      </ul>
+      <ul>
+        <li>
           <span className='fkt-who'><a href='https://zieloniwpodrozy.pl/' target='_blank'>Mateusz Stawarz</a></span>
           <span className='fkt-how-much'>18d 9h 45m</span>
           <span className='fkt-when'>Wrzesień, 2021</span>
           <span className='fkt-url'><a href='https://www.youtube.com/watch?v=QZlnyDc-Pco&list=PLNVscyeSGA6Bbe-DBFsYsPUdfbKYPMWtI' target='_blank'>Relacja filmowa</a></span>
+        </li>
+      </ul>
+      <ul>
+        <li>
+          <span className='fkt-who'><a href='' target='_blank'>Marcin Drozdowski</a></span>
+          <span className='fkt-how-much'>19d 10h</span>
+          <span className='fkt-when'>Maj, 2022</span>
+        </li>
+      </ul>
+      <ul>
+        <li>
+          <span className='fkt-who'><a href='' target='_blank'>Ania Ca</a></span>
+          <span className='fkt-how-much'>Na raty</span>
+          <span className='fkt-when'>Jesień, 2021</span>
         </li>
       </ul>
     </div>
